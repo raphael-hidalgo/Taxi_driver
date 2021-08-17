7dfcf0c7bd8eeb22dd36a7758bf7037d1d65c9bc
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2021-08-17 14:32:45 +0200

    4th all good

--- ml_flow_test.py
+++ ml_flow_test.py
@@ -1,12 +1,28 @@
+import mlflow
 from mlflow.tracking import MlflowClient
-EXPERIMENT_NAME = "test_experiment"
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = '[FR] [Paris] [raphael-hidalgo] TaxiFareModel + 1.0'
+
+
+# Indicate mlflow to log to remote server
+mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+
 client = MlflowClient()
+
 try:
     experiment_id = client.create_experiment(EXPERIMENT_NAME)
 except BaseException:
-    experiment_id = client.get_experiment_by_name(EXPERIMENT_NAME).experiment_id
+    experiment_id = client.get_experiment_by_name(
+        EXPERIMENT_NAME).experiment_id
+
+yourname = 'Raphaël Hidalgo'
+
+if yourname is None:
+    print("please define your name, il will be used as a parameter to log")
 
 for model in ["linear", "Randomforest"]:
     run = client.create_run(experiment_id)
     client.log_metric(run.info.run_id, "rmse", 4.5)
     client.log_param(run.info.run_id, "model", model)
+    client.log_param(run.info.run_id, "student_name", yourname)
